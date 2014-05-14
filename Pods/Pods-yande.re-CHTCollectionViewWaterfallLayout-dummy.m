3fca174f500a3ced648c13a775addf0b2e072f09
Zuyang Kou <leafduo@gmail.com>
2014-05-15 02:35:01 +0800

    Remove Pods/

--- Pods/Pods-yande.re-CHTCollectionViewWaterfallLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_yande_re_CHTCollectionViewWaterfallLayout : NSObject
-@end
-@implementation PodsDummy_Pods_yande_re_CHTCollectionViewWaterfallLayout
-@end
--- Pods/Pods-yande.reTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_yande_reTests : NSObject
-@end
-@implementation PodsDummy_Pods_yande_reTests
-@end
